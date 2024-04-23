1be166d40112d02ffa20edb4ff5938eae809e201
Wayne-Cole <77279425+Wacky404@users.noreply.github.com>
2024-04-22 20:55:21 -0500

    add: updated grid file for creation of matrix

--- grid.py
+++ grid.py
@@ -6,9 +6,14 @@ import sys
 np.set_printoptions(threshold=np.inf)
 
 
+def _get_diff(inpt_one: list, sub_inpt: list) -> list[int, int]:
+    """ helper function to get diff of two [n, n] lists """
+    # column integer is negative for now, might need to change
+    return [int(inpt_one[0] - sub_inpt[0]), -abs(inpt_one[1] - sub_inpt[1])]
+
+
 def _locate_mid(grid: np.ndarray) -> list:
     """ helper function to locate middle of grid """
-    # have to use this on the traversable area grid
     shape: list = list(grid.shape)
     for i, _ in enumerate(shape):
         shape[i] = shape[i] - int(2)
@@ -28,6 +33,39 @@ def _locate_mid(grid: np.ndarray) -> list:
     return [row_pos, col_pos]
 
 
+def gen_paths(grid: np.ndarray) -> np.ndarray:
+    # works only for treat position in top left of matrix
+    treat_pos: list[list] = [
+        [0, 0]
+    ]
+    for treat in treat_pos:
+        start: list = treat
+        get_to: list = _locate_mid(grid=grid)
+
+        go: bool = True
+        while go:
+            choice: str = str(random.choice(['down', 'right']))
+            print('Choice: ', choice)
+            if str(choice) == 'down':
+                if start[0] < get_to[0]:
+                    start[0] = int(start[0]) + 1
+                    if start != get_to:
+                        print(start, get_to)
+                        grid[1:-1, 1:-1][start[0],
+                                         start[1]] = np.byte(9)
+            if str(choice) == 'right':
+                if start[1] < get_to[1]:
+                    start[1] = int(start[1]) + 1
+                    if start != get_to:
+                        print(start, get_to)
+                        grid[1:-1, 1:-1][start[0], start[1]] = np.byte(9)
+            if start == get_to:
+                print(start, get_to)
+                go = False
+
+    return grid
+
+
 def gen_treats(grid: np.ndarray) -> np.ndarray:
     """ generates treats at each corner of traversable area, using 4 """
     # generate the treats in the four corners of the map
@@ -44,8 +82,6 @@ def gen_treats(grid: np.ndarray) -> np.ndarray:
 
 def gen_obstacles(grid: np.ndarray) -> np.ndarray:
     """ randomly generates obstacles, using 1 """
-    zero: np.byte = np.byte(0)
-    one: np.byte = np.byte(1)
     with np.nditer(grid[1:-1, 1:-1], op_flags=['readwrite']) as ar:
         for x in ar:
             x[...] = random.choice([np.byte(1), np.byte(0)])
@@ -69,7 +105,9 @@ def convert_boundaries(grid: np.ndarray) -> np.ndarray:
 def create_grid(row: int = 25, column: int = 33) -> np.ndarray:
     """ creates a grid using np.ndarray zeros """
     # min grid size 5x10
-    grid: np.ndarray = np.zeros(shape=(row, column), dtype=np.byte, order='c')
+    grid: np.ndarray = np.zeros(
+        shape=(row, column), dtype=np.byte, order='c')
+
     return grid
 
 
@@ -80,4 +118,5 @@ if __name__ == "__main__":
     grid = gen_treats(grid=grid)
     position: list = _locate_mid(grid=grid)
     grid[position[0], position[1]] = np.byte(7)
-    print(grid)
+    print(gen_paths(grid=grid))
+    print(position)
